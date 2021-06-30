937466bb4540c6d785677cb598807b7a704ded2a
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-06-30 10:47:54 +0300

    Updates

---
+++ precache-manifest.72a2d93d4cd749c3abb25e4a26488a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53f57677a10abb025befbac604a0247",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "6a3bf33c305269b7517b",
+    "url": "/slider/static/css/main.c31a1628.chunk.css"
+  },
+  {
+    "revision": "68a145420c583c8e54b0",
+    "url": "/slider/static/js/2.afca9a48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slider/static/js/2.afca9a48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3bf33c305269b7517b",
+    "url": "/slider/static/js/main.739df603.chunk.js"
+  },
+  {
+    "revision": "ce1caf34cf0aba241fa2",
+    "url": "/slider/static/js/runtime-main.c6fd19b8.js"
+  }
+]);
